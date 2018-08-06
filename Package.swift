9e3608f029ec7d6627a76a53a07d5d7f15221176
chieryw <chieryw@163.com>
2018-08-06 23:44:12 +0800

    增加line

--- Package.swift
+++ Package.swift
@@ -5,15 +5,16 @@ import PackageDescription
 
 let package = Package(
     name: "WebService",
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
+    dependencies:  [
+        .package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", from: "3.0.0"),
+//        .package(url: "https://github.com/PerfectlySoft/Perfect-MySQL.git", from: "3.0.0"),
+//        .package(url: "https://github.com/PerfectlySoft/Perfect-RequestLogger.git", from: "3.0.0"),
+//        .package(url: "https://github.com/SwiftORM/MySQL-StORM.git", from: "3.0.0"),
+//        .package(url: "https://github.com/PerfectlySoft/Perfect-Session-MySQL.git", from: "3.0.0"),
+        ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "WebService",
-            dependencies: []),
+            dependencies: ["PerfectHTTPServer"/*, "PerfectMySQL", "PerfectRequestLogger", "MySQLStORM", "PerfectSessionMySQL"*/]),
     ]
 )
--- Sources/WebService/main.swift
+++ Sources/WebService/main.swift
@@ -1,1 +1,112 @@
-print("Hello, world!")
+import PerfectHTTP
+import PerfectHTTPServer
+
+
+// 注册您自己的路由和请求／响应句柄
+var routes = Routes()
+
+let handler = {
+    (request: HTTPRequest, response: HTTPResponse) in
+    
+    print("params")
+    print("-----------")
+    print(request.postParams)
+    
+    print("postBodyBytes")
+    print("-----------")
+    print(request.postBodyBytes ?? "")
+    
+    print("postBodyString")
+    print("-----------")
+    print(request.postBodyString ?? "")
+    
+    print("postFileUploads")
+    print("-----------")
+    print(request.postFileUploads ?? "")
+    
+    
+    if let decodedJson = try? request.postBodyString?.jsonDecode() as? [String: String] {
+        let callStack = decodedJson!["callStack"]
+        let type = decodedJson!["type"]
+        let name = decodedJson!["name"]
+        let reason = decodedJson!["reason"]
+        let appInfo = decodedJson!["appInfo"]
+        let topVC = decodedJson!["topVC"]
+        
+        
+        response.setHeader(.contentType, value: "application/json")
+        response.appendBody(string: "{\"a\":\"b\"}")
+            .completed()
+    }
+    else {
+        
+    }
+    
+    response.setHeader(.contentType, value: "application/json")
+    response.appendBody(string: "{\"a\":\"b\"}")
+        .completed()
+}
+
+let putHandler = {
+    (request: HTTPRequest, response: HTTPResponse) in
+    
+    print("params")
+    print("-----------")
+    print(request.postParams)
+    
+    print("postBodyBytes")
+    print("-----------")
+    print(request.postBodyBytes ?? "")
+    
+    print("postBodyString")
+    print("-----------")
+    print(request.postBodyString ?? "")
+    
+    print("postFileUploads")
+    print("-----------")
+    print(request.postFileUploads ?? "")
+    
+    if let decodedJson = try? request.postBodyString?.jsonDecode() as? [String: String] {
+        let callStack = decodedJson!["callStack"]
+        let type = decodedJson!["type"]
+        let name = decodedJson!["name"]
+        let reason = decodedJson!["reason"]
+        let appInfo = decodedJson!["appInfo"]
+        let topVC = decodedJson!["topVC"]
+        
+        
+        response.setHeader(.contentType, value: "application/json")
+        response.appendBody(string: "{\"a\":\"b\"}")
+            .completed()
+    }
+    else {
+        
+    }
+    
+    response.setHeader(.contentType, value: "application/json")
+    response.appendBody(string: "{\"a\":\"b\"}")
+        .completed()
+}
+
+routes.add(method: .put, uri: "/put", handler: putHandler)
+
+routes.add(method: .post, uri: "/upload", handler: handler)
+
+routes.add(method: .get, uri: "/temp") {
+    request, response in
+    
+    response.setHeader(.contentType, value: "application/json")
+    response.appendBody(string: "{\"a\":\"b\"}")
+        .completed()
+    //    response.setHeader(.contentType, value: "text/html")
+    //    response.appendBody(string: "<html><title>Hello, world!</title><body>Hello, world!</body></html>")
+    //        .completed()
+}
+
+do {
+    // 启动HTTP服务器
+    try HTTPServer.launch(
+        .server(name: "www.example.ca", port: 8181, routes: routes))
+} catch {
+    fatalError("\(error)") // fatal error launching one of the servers
+}
